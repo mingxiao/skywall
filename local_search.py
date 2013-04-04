cfca2c8f977b07e8256944918aadc80b34a23aed
ming <ubuntu@ming-ubuntu.(none)>
2013-04-04 15:31:48 -0700

    Add mysql functionality

--- local_search.py
+++ local_search.py
@@ -8,10 +8,32 @@ while c not within tolerance:
 
 The configuration cost is something that was computed before-hand thus we
 cans simlpy obtain it. Haven't yet decided on the format, but it will most likely
-be a database. I elect to use sqlite as the database since python has a builtin
-sqlite3 module.
-"""
+be a database. I elect to use mysql since labview will be using mysql as well.
 
+For testing and development purposes we can setup dummy tables.
+"""
+import _mysql
+    
+host = 'localhost'
+user = 'root'
+userpass = 'rootiam'
+db = 'test'
+con = None
+
+def connect(host,usr,upass, db):
+    global con
+    con = _mysql.connect(host,usr,upass,db)
+
+def _get_sim_value(fid, sid, diml):
+    global con
+    assert con is not None
+    """
+    Get the SIMULATED sensor value of sensor sid when fixture fid is at dimming level diml
+    """
+    con.query('select s%s from F%s_SIM where diml = %s'%(sid,fid,diml))
+    result = con.use_result()
+    return result.fetch_row()[0][0]
+        
 def initial_guess():
     """
     Returns the inital configuration guess. Do not know the form of the
@@ -45,6 +67,9 @@ def best_neighbor(nbors):
     raise NotImplementedError
 
 if __name__ == '__main__':
+    connect(host,user,userpass,db)
+    print con
+    print _get_sim_value(1,2,0)
     pass
 
 
