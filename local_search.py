da4ba54b8640b589f6d501ffd7f650da7b4d92c0
ming <ubuntu@ming-ubuntu.(none)>
2013-04-12 13:11:38 -0700

    Add functionality to get reading from table

--- local_search.py
+++ local_search.py
@@ -14,15 +14,22 @@ For testing and development purposes we can setup dummy tables.
 
 
 TODO:
-What is the structure of the configurations? List, dictionary? Probably dictionary
+What is the structure of the configurations? List, dictionary?
+Configurations are dictionaries that map from fixtureID --> Dimming Level
+
+We have no guarantee on the numbering of the fixtureID, so we need a non-constrainted
+way of getting values.
 """
 import _mysql
-    
+import math
+
 host = 'localhost'
 user = 'root'
 userpass = 'rootiam'
 db = 'test'
 con = None
+configType = dict
+sensors = [1,2,3] #for testing there are only 3 sensors
 
 def connect(host,usr,upass, db):
     global con
@@ -41,18 +48,25 @@ def _get_sim_value(fid, sid, diml):
     @parad sid - sensor id, int
     @param diml - dimming level, int
     """
-    con.query('select s%s from F%s_SIM where diml = %s'%(sid,fid,diml))
-    result = con.use_result()
-    return result.fetch_row()[0][0]
+    try:
+        con.query('select s%s from F%s_SIM where diml = %s'%(sid,fid,diml))
+        result = con.use_result()
+        return result.fetch_row()[0][0]
+    except _mysql.ProgrammingError:
+        raise _mysql.ProgrammingError, 'Query malformed, check table name'
 
 def _least_squares(truth, simul):
     """
+    TODO: redo this so the inputs are dictionaries!
+    
     Return the least squares of two list of numbers
     
     @param truth - list of ordered observed sensor readings
     @param simul - list of ordered simulated readings
     """
     assert len(truth) == len(simul)
+    diff = [math.pow(x - y,2) for x,y in zip(truth,simul)]
+    return sum(diff)
     
 def initial_guess():
     """
@@ -73,14 +87,60 @@ def neighbors(config):
     """
     raise NotImplementedError
 
+def _get_sim_single_fix(fix_id, diml):
+    """
+    Returns a dictionary that maps from fix_id --> {sensor_id --> reading}, based on the dimming
+    level diml
+    """
+    global sensors
+    d = {}
+    for s in sensors:
+        val = _get_sim_value(fix_id,s, diml)
+        d[s] = val
+    return d
+
+def _get_sim_all_fix(config):
+    """
+    Given a dictinary of fixtureID --> dimming levels, return a dictionary within a dictionary
+    that maps from fixtureID --> {sensorID --> reading}  (based on that fixure's dimming level)
+    """
+    global configType
+    assert type(config) == configType
+    d = {}
+    for fixture in config.keys():
+        val = _get_sim_single_fix(fixture,config[fixture])
+        d[fixture] = val
+    return d
+
+def _sum_sensor_readings(sreadings):
+    """
+    sreadings is a dictionary of dictionaries mapping from fixtureID --> {sensorID --> reading}
+
+    Sum all the readings according to sensorID and return a dictionary mapping from
+    sensorID --> sum of reading values
+    """
+    global sensors
+    d ={}
+    val = 0
+    for sensor in sensors:
+        sval = 0
+        for fixtures in sreadings.iterkeys():
+            sval += sreadings[fixtures].get(sensor,0)
+        d[sensor] = sval
+    return d
+
 def cost(config):
     """
     Returns the cost of configuration config. The cost is the least squares of the simulated
     values from the ground truth
     """
+    global configType
+    assert type(config) == configType
     #get simulated sensor readings for sensor per fixture
-    #sum over all fixtures
-    #get least squares
+    sim_values = _get_sim_all_fix(config)
+    #sum over all fixtures, simulated values
+    #get the ground truth sensor readings
+    #output least squares
     raise NotImplementedError
 
 def best_neighbor(nbors):
@@ -93,7 +153,6 @@ if __name__ == '__main__':
     connect(host,user,userpass,db)
     print con
     print _get_sim_value(1,2,0)
-    pass
 
 
 
--- test.py
+++ test.py
@@ -2,9 +2,41 @@
 Unit test script for local_search.py
 """
 import unittest
+import local_search as ls
+import _mysql as sql
+
 
 class Test(unittest.TestCase):
-    pass
+    def setUp(self):
+        #setup database connection
+        host = 'localhost'
+        user = 'root'
+        password = 'rootiam'
+        database = 'test'
+        ls.connect(host,user,password,database)
+        assert ls.con is not None
 
+    def test_least_squares(self):
+        a = [1,2,3,4,5]
+        b = [3,7,12,9,8]
+        c = [0,-3,1,1,1]
+        assert ls._least_squares(a,b) == 144
+        assert ls._least_squares(a,a) == 0
+        assert ls._least_squares(a,c) == 55
+
+    def test_get_sim_single_fix(self):
+        self.assertEqual(ls._get_sim_single_fix(1,0),{1: '3.4', 2: '5.5', 3: '10'})
+
+    def test_get_sim_all_fix(self):
+        d = {1:0,2:10}
+        ans = ls._get_sim_all_fix(d)
+        for key,val in ans.iteritems():
+            assert type(val) == dict
+            assert type(key) == int
+
+    def test_sum_sensor_readings(self):
+        config = {1:{1:10,2:20,3:30},2:{1:3,2:7,3:1}}
+        self.assertEqual(ls._sum_sensor_readings(config),{1:13,2:27,3:31})
+        
 if __name__ == '__main__':
     unittest.main()
